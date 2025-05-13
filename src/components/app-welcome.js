e7fb031a6cfcae4de667aaf0aca6d49fe0fdbdc5
jcristancho2 <jocristanchool@gmail.com>
2025-05-12 19:14:24 -0500

    feat: ✨  implementacion modulo de  welcomeview

---
+++ src/components/app-welcome.js
@@ -0,0 +1,11 @@
+export class Welcomeview extends HTMLElement{
+    constructor(){
+        super();
+        this.render();
+    }
+async render(){
+
+}
+}
+
+customElements.define("welcome-view", null);
